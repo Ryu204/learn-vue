dd11e3f0655157db2e6eee6d49b1eff084c240ca
Ryu204 <nguyenanhbao2356@gmail.com>
2024-06-04 22:10:45 +0700

    enhance wallet info card (add clipboard)

--- src/components/WalletDetail.vue
+++ src/components/WalletDetail.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import Card from 'primevue/card'
+import Button from 'primevue/button'
+import { useToast } from 'primevue/usetoast'
 import provider from '../scripts/provider'
 import metamaskIcon from '../assets/metamask.svg'
 import trustwalletIcon from '../assets/trustwallet.svg'
@@ -7,8 +9,17 @@ import ConnectWalletButton from './ConnectWalletButton.vue'
 
 const info = provider.walletInfo
 const unit = import.meta.env.VITE_TOKEN_NAME
+const toast = useToast()
 
-const lol = '800px'
+async function copyTokenAddress() {
+    await navigator.clipboard.writeText(info.tokenAddress)
+    toast.add({
+        severity: 'success',
+        summary: 'Copied',
+        detail: `"${info.tokenAddress.slice(0, 10) + '...'}" has been copied to clipboard.`,
+        life: 3000
+    })
+}
 
 </script>
 
@@ -18,14 +29,18 @@ const lol = '800px'
         <template #content>
             <div class="flex-row">
                 <div>
-                    <p>Token address: {{ info.tokenAddress }}</p>
+                    <p>
+                        Token address: {{ info.tokenAddress }}
+                        <Button icon="pi pi-clone" @click="copyTokenAddress" class="copy"></Button>
+                    </p>
                     <p>Balance: {{ info.balance }} {{ unit }}</p>
                     <p>Token vesting: {{ info.tokenVesting }} {{ unit }}</p>
                     <p>Claimed: {{ info.claimed }} {{ unit }}</p>
                 </div>
                 <div class="flex-column">
                     <ConnectWalletButton :logo="metamaskIcon" name="MetaMask" class="inline"></ConnectWalletButton>
-                    <ConnectWalletButton :logo="trustwalletIcon" name="Trust Wallet" class="inline"></ConnectWalletButton>
+                    <ConnectWalletButton :logo="trustwalletIcon" name="Trust Wallet" class="inline">
+                    </ConnectWalletButton>
                 </div>
             </div>
         </template>
@@ -50,6 +65,15 @@ const lol = '800px'
     display: inline-block;
     align-self: center;
     align-items: center;
+}
+
+.copy {
+    padding: 0;
+    margin-left: 5px;
+    background-color: transparent;
+    color: white;
+    max-width: 20px;
+    border: none;
 }
 
 @media screen and (max-width: 800px) {
