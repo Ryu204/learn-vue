b86dbcce820bf757e7d9eb3610f08f2d83d27cb4
Ryu204 <nguyenanhbao2356@gmail.com>
2024-06-05 21:05:34 +0700

    fix toast and text overflow

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,24 @@
 import NavBar from './components/NavBar.vue'
 import WalletDetail from './components/WalletDetail.vue'
 import VestingTimeline from './components/VestingTimeline.vue'
+import Footer from './components/Footer.vue'
 import Toast from 'primevue/toast'
 
 import provider from './scripts/provider'
 </script>
 
 <template>
-  <div>
-    <Toast></Toast>
-  </div>
-  <header>
-    <NavBar></NavBar>
-  </header>
-  <main>
-    <WalletDetail v-if="provider.isLoggedIn.value" class="margin-bottom-large"></WalletDetail>
-    <VestingTimeline></VestingTimeline>
-  </main>
+    <div>
+        <Toast></Toast>
+    </div>
+    <header>
+        <NavBar></NavBar>
+    </header>
+    <main>
+        <WalletDetail v-if="provider.isLoggedIn.value" class="margin-bottom-large"></WalletDetail>
+        <VestingTimeline></VestingTimeline>
+    </main>
+    <Footer></Footer>
 </template>
 
 <style scoped>
---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const unit = import.meta.env.VITE_TOKEN_NAME
+
+</script>
+
+<template>
+    <div class="footer">
+        <p>Vesting page © {{ unit }} 2024</p>
+        <p>Powered by sPhoton</p>
+    </div>
+</template>
